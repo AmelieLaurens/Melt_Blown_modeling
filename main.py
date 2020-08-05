f3217144c10027e79649208b6baf4ea311905012
AmelieLaurens <66320985+AmelieLaurens@users.noreply.github.com>
2020-08-05 16:13:12 +0200

    updates

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 Created on Mon Jun 29 10:55:08 2020
 
 @author: amelielaurens
+
+Reference : Modeling Polymer Air Drawing in the Melt Blowing Nonwoven Process
+by Chen et Huang, 2003
+
 """
 
 import numpy
@@ -11,12 +15,13 @@ import matplotlib.pyplot as plt
 from math import *
 from models import *
 
-# Discrétisation
-N = 5 #Plus N est grand plus la discrétisation est fine
-x_hat = numpy.linspace(0, 20, N)
-delta_x = x_hat[1] - x_hat[0]
+# Discretisation
+N = 20   # The higher the discretisation number N is, the finer the discretisation will be,
+        # there will be more points on the graphic.
+x_position = numpy.linspace(0, 20, N)
+delta_x = x_position[1] - x_position[0]
 
-# Définition des constantes
+# Constants initialization
 rho = 900.
 pol_flow_rate = 0.000012
 gravitational_acceleration = 9.81
@@ -28,8 +33,9 @@ ua = 25.7
 va = 1.169*10**(-5)
 beta=0.78
 cst_n = 0.61
+coef_j = numpy.linspace(-1, 1, N)
 
-# Initialisation : on rentre les conditions initiales
+# Initialization : enter the initial conditions
 Fiber_diameter = [0.000407]
 Cxx = [366757.]
 Cyy = [-183378.]
@@ -37,8 +43,8 @@ Fr = [0.007335137]
 Re = [275.3]
 Cf = [0.0253403]
 fiber_velocity = [4*pol_flow_rate/(pi*Fiber_diameter[0]**2*rho)]
-coef_j = numpy.linspace(-1, 1, N)
 
+# Compute datas
 for k in range(N):
     fiber_velocity.append(4*pol_flow_rate/(pi*Fiber_diameter[k]**2*rho))
     Cxx.append(constitutive_equation_Cxx(eta, cst_m, fiber_velocity[k], fiber_velocity[k+1], N))
@@ -46,18 +52,20 @@ for k in range(N):
     Fr.append(rheological_force(Fiber_diameter[k], Cxx[k], Cyy[k]))
     Re.append(Reynolds(Fiber_diameter[k], ua, fiber_velocity[k], va))
     Cf.append(Drag_coefficient(Re[k], beta, cst_n))
-    Fiber_diameter.append(momentum_equation(Fiber_diameter[k], delta_x, rho, pol_flow_rate, Fr, gravitational_acceleration, Cf, rho_a, ua, coef_j[k], k, fiber_velocity[k]))
+    Fr1 = Fr[k]
+    Fr2 = Fr[k+1]
+    Fiber_diameter.append(momentum_equation(Fiber_diameter[k], delta_x, rho, pol_flow_rate, Fr1, Fr2, gravitational_acceleration, Cf[k], rho_a, ua, coef_j[k], fiber_velocity[k]))
     
-Fiber_diameter=numpy.array(Fiber_diameter)
-#import pdb; pdb.set_trace()
+Fiber_diameter = numpy.array(Fiber_diameter)
 
+# Draw the figure 
 fig = plt.figure()
 axes = fig.add_subplot(1, 1, 1)
 
 for i in range(N):
-    axes.plot(x_hat[i], Fiber_diameter[i], 'ro')
+    axes.plot(x_position[i], Fiber_diameter[i], 'ro')
 axes.grid()
 axes.set_xlabel("Axial position x (m)", fontsize=16)
 axes.set_ylabel("Fiber diameter (m)", fontsize=16)
 axes.set_title("Fiber diameter as a function of the axial position ", fontsize=16)
-
+plt.show()
--- models.py
+++ models.py
@@ -4,6 +4,10 @@
 Created on Tue Jun 23 16:03:49 2020
 
 @author: amelielaurens
+
+Reference : Modeling Polymer Air Drawing in the Melt Blowing Nonwoven Process
+by Chen et Huang, 2003
+
 """
 
 from math import *
@@ -26,83 +30,114 @@ def continuity_equation(G, u, rho):
     return sqrt(4*G/(pi*u*rho))
 
 def rheological_force(D, Cxx, Cyy):
-    return D**2*pi*(Cxx-Cyy)/4
     """Melt Blown Rheological force
 
     Fr=D**2*pi*(Cxx-Cyy)/4
 
-     :Input:
-     - *D* (float) - fiber diameter (m)
-     - *Cxx* (float) - axial tensile stress of polymer
-     - *Cyy* (float) - transversal tensile stress of polymer
+    :Input:
+    - *D* (float) - fiber diameter (m)
+    - *Cxx* (float) - axial tensile stress of polymer
+    - *Cyy* (float) - transversal tensile stress of polymer
 
-     :Returns:
-     (float) - rheological force (N)
+    :Returns:
+    (float) - rheological force (N)
 
-     """
+    """
+    return D**2*pi*(Cxx-Cyy)/4
 
 def Reynolds(D, ua, u, va):
-
     """Melt Blown Reynolds number
 
     Re=D*abs(ua-u)/va
 
-     :Input:
-     - *D* (float) - fiber diameter (m)
-     - *ua* (float) - x-component of air velocity (m/s)
-     - *u* (float) - fiber velocity (m/s)
-     - *va* (float) - air kinematic viscosity (m^2/s)
+    :Input:
+    - *D* (float) - fiber diameter (m)
+    - *ua* (float) - x-component of air velocity (m/s)
+    - *u* (float) - fiber velocity (m/s)
+    - *va* (float) - air kinematic viscosity (m^2/s)
 
-     :Returns:
-     (float) - Reynolds number
+    :Returns:
+    (float) - Reynolds number
 
-     """
+    """
     return D*abs(ua-u)/va
 
 def Nusselt(Re, gamma, k1):
-    return gamma*Re**k1
-
     """Melt Blown Nusselt number
 
     Nu = gamma*Re**k1
 
-     :Input:
-     - *Re* (float) - Reynolds number
-     - *gamma* (float) - constant
-     - *k1* (float) - constant
+    :Input:
+    - *Re* (float) - Reynolds number
+    - *gamma* (float) - constant
+    - *k1* (float) - constant
 
-     :Returns:
-     (float) - Nusselt number
+    :Returns:
+    (float) - Nusselt number
 
-     """
+    """
+    return gamma*Re**k1
 
 def Drag_coefficient(Rey, beta, n):
+    """Melt Blown Drag coefficient
+
+    Cf=beta*Re**(-n)
+
+    :Input:
+    - *Re* (float) - Reynolds number (m)
+    - *beta* (float) - constant of Matsui’s relation
+    - *n* (float) - constant of Matsui’s relation
+
+    :Returns:
+    (float) - Drag coefficient
+
+    """
     if Rey == 0 :
         return 10000
     else:
         return beta*Rey**(-n)
-    """Melt Blown Drag_coefficient
-
-    Cf=beta*Re**(-n)
-
-     :Input:
-     - *Re* (float) - Reynolds number (m)
-     - *beta* (float) - constant of Matsui’s relation
-     - *n* (float) - constant of Matsui’s relation
-
-     :Returns:
-     (float) - Drag coefficient
-
-     """
 
 
-def momentum_equation(fiber_diameter, delta_x, rho, pol_flow_rate, Fr, gravitational_acceleration, Cf, rho_a, ua, coef_j, k, fiber_velocity):
-        return fiber_diameter+delta_x*pi*rho*fiber_diameter**3/(8*pol_flow_rate**2)*((Fr[k]-Fr[k+1])/delta_x-coef_j*pi*fiber_diameter*Cf[k]*rho_a/2*(ua-fiber_velocity)**2-pi*fiber_diameter**2*rho*gravitational_acceleration/4)
+def momentum_equation(fiber_diameter, delta_x, rho, pol_flow_rate, Fr1, Fr2, gravitational_acceleration, Cf, rho_a, ua, coef_j, fiber_velocity):
+    """Melt Blown Momentum equation
+
+    :Input:
+    - *fiber_diameter* (float) - fiber diameter (m)
+    - *delta_x* (float) - dicretisation on the axis x : delta_x = x_position[1] - x_position[0]
+    - *rho* (float) - polymer density (kg/m^3)
+    - *pol_flow_rate* (float) - polymer flow rate (kg/s)
+    - *Fr1* (float) - Rheological force (N) at the position x(n)
+    - *Fr2* (float) - Rheological force (N) at the position x(n+1) = x(n) + delta_x
+    - *gravitational_acceleration* (float) - gravitational acceleration (m/s^2)
+    - *Cf* (float) - Drag coefficient / air drawing coefficient
+    - *rho_a* (float) - air density (kg/m^3)
+    - *ua* (float) - x-component of air velocity (m/s)
+    - *coef_j* (float) - factor j : this factor deals with the fact that near the spinneret, the airflow
+    acts as a positive (downward) force on the polymer melt, but on the far side of the spinneret, 
+    the force is negative.
+    - *fiber_velocity* (float) -  Fiber velocity (m/s)
+
+    :Returns:
+    (float) - Fiber diameter (m) (at a position n+1)
+
+    """
+    return fiber_diameter+delta_x*pi*rho*fiber_diameter**3/(8*pol_flow_rate**2)*((Fr1-Fr2)/delta_x+coef_j*pi*fiber_diameter*Cf*rho_a/2*(ua-fiber_velocity)**2-pi*fiber_diameter**2*rho*gravitational_acceleration/4)
 
 
-def constitutive_equation_Cxx(eta, cst_m, fiber_velocity1, fiber_velocity2, N):
-    x_hat = numpy.linspace(0, 20, N)
-    delta_x = x_hat[1] - x_hat[0]
+def constitutive_equation_Cxx(eta, cst_m, fiber_velocity1, fiber_velocity2, delta_x):
+    """Melt Blown constitutive equation Cxx
+
+    :Input:
+    - *eta* (float) - Shear viscosity (Pa.s)
+    - *cst_m* (float) - Power-law exponent
+    - *fiber_velocity1* (float) - Fiber velocity (m/s) at the position x(n)
+    - *fiber_velocity2* (float) - Fiber velocity (m/s) at the position x(n+1) = x(n) + delta_x
+    - *delta_x* (float) - dicretisation on the axis x : delta_x = x_position[1] - x_position[0]
+   
+    :Returns:
+    (float) - Axial tensile stress of polymer Cxx (Pa)
+
+    """
     if fiber_velocity1==fiber_velocity2:
         return 0
     else:
@@ -110,9 +145,20 @@ def constitutive_equation_Cxx(eta, cst_m, fiber_velocity1, fiber_velocity2, N):
         return 2*eta*(u_prime)**cst_m
 
 
-def constitutive_equation_Cyy(eta, cst_m, fiber_velocity1, fiber_velocity2, N):
-    x_hat = numpy.linspace(0, 20, N)
-    delta_x = x_hat[1] - x_hat[0]
+def constitutive_equation_Cyy(eta, cst_m, fiber_velocity1, fiber_velocity2, delta_x):
+    """Melt Blown constitutive equation Cyy
+
+    :Input:
+    - *eta* (float) - Shear viscosity (Pa.s)
+    - *cst_m* (float) - Power-law exponent
+    - *fiber_velocity1* (float) - Fiber velocity (m/s) at the position x(n)
+    - *fiber_velocity2* (float) - Fiber velocity (m/s) at the position x(n+1) = x(n) + delta_x
+    - *delta_x* (float) - dicretisation on the axis x : delta_x = x_position[1] - x_position[0]
+   
+    :Returns:
+    (float) - Transversal tensile stress of polymer Cyy (Pa)
+
+    """
     if fiber_velocity1==fiber_velocity2:
         return 0
     else:
